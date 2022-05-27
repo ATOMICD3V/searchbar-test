4374265b26ff9fa07e44351352c811777619cadd
rip404dev <97807507+ATOMICD3V@users.noreply.github.com>
2022-05-26 21:05:17 -0600

    Update and rename get_results.php to index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+<html>
+<head>
+<title>Title of your search engine - pakainfo.com</title>
+</head>
+<body>
+<form action='search.php' method='GET'>
+<center>
+<h1>Pakainfo - Search Engine</h1>
+<input type='text' size='90' name='search'></br></br>
+<input type='submit' name='submit' value='You have simple Search source code' ></br></br></br>
+</center>
+</form>
+</body>
+</html>
